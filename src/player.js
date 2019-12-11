e6e8defb7444771555f98bc504b520af811867ad
Patrick Gaskill <patrick.gaskill@enfusionsystems.com>
2019-12-11 12:49:35 -0600

    WIP

--- src/player.js
+++ src/player.js
@@ -1,11 +1,38 @@
+"use strict";
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://orteil.dashnet.org/cookieclicker/", {
-    waitUntil: "networkidle2"
+    waitUntil: "networkidle0"
   });
 
-  await browser.close();
+  // Click the "Here you go" achievement
+  // await page.evaluate(() => window.Game.AchievementsById[204].click());
+
+  // If golden cookie is available, click it
+  // Click it on the last second it's available?
+  // setInterval(() => {}, 1000);
+
+  // If a sugar lump is available, click it
+  // setInterval(() => {
+  //   const lumps = page.$("#lumps");
+  //   console.log("lumps:", lumps);
+  //   if (lumps) lumps.click();
+  // });
+
+  // Find all available upgrades
+
+  // Find all available buildings
+
+  // Calculate the best thing to buy
+
+  // Click the cookie
+  const bigCookie = await page.$("#bigCookie");
+  setInterval(() => {
+    bigCookie.click();
+  }, 10);
+
+  // Update cookies, CPS, achievements
 })();
